5661d58784ee0ba3cd3a21b54155e564e98b63c9
THEATE Jérome <jerome.theate@grenoble-inp.org>
2019-01-18 13:51:46 +0100

    optimisation de l'affichage des graphe avec bouton submit

--- nodeJS/public/js/test.js
+++ nodeJS/public/js/test.js
@@ -2,8 +2,8 @@ $.get('irrigation', function(data, status){
   json = JSON.parse(data);
   for (let i=0; i<json.length; i++){
     var div = "<div class='controlableSystems'><div class='controlableSystem'>";
-    var name = "<div style='float:left;width:50%;text-align:right; " +
-    "padding-right:5%'>Irrigation sur station " + json[i].id_station + "</div>";
+    var name = "<div style='float:left;width:60%;text-align:right; " +
+    "padding-right:2%'>Irrigation sur station " + json[i].id_station + "</div>";
     var checkbox = '<label class="switch"><input id="station' +
     json[i].id_station + '" type="checkbox">' +
     '<div class="slider round"></div></label>';
@@ -125,15 +125,17 @@ function generateGrafanaUrl(start, end){
   return url
 }
 
-// Gestion evenement modification du select de la temperature
-function onTemperatureSelected(){
+
+// Gestion evenement modification du select de l'humidité ou temperature
+function onGraphSelected(type, ref){
+
   // On récupère les valeurs des différents select.
-  let v1 = $('select#temperatureFromWeek').val(); // valeur du select
-  let v2 = $('select#temperatureFromDay').val(); // valeur du select
-  let v3 = $('select#temperatureFromHour').val(); // valeur du select
-  let v4 = $('select#temperatureToWeek').val(); // valeur du select
-  let v5 = $('select#temperatureToDay').val(); // valeur du select
-  let v6 = $('select#temperatureToHour').val(); // valeur du select
+  let v1 = $('select#' + type + 'FromWeek').val(); // valeur du select
+  let v2 = $('select#' + type + 'FromDay').val(); // valeur du select
+  let v3 = $('select#' + type + 'FromHour').val(); // valeur du select
+  let v4 = $('select#' + type + 'ToWeek').val(); // valeur du select
+  let v5 = $('select#' + type + 'ToDay').val(); // valeur du select
+  let v6 = $('select#' + type + 'ToHour').val(); // valeur du select
 
   // On calcul les temps en heure donnés par l'utilisateur
   //    start : nombre d'heure dans le passé
@@ -141,65 +143,28 @@ function onTemperatureSelected(){
   //    end : nombre d'heure dans le passé devant être inférieur à 'start'
   let end = (parseInt(v4) * 7 * 24) + (parseInt(v5) * 24) + parseInt(v6);
 
+  // console.log('given start date : ', start);
+  // console.log('given end date : ', end);
   // Si end > start, on inverse les deux
   if (end > start) {
-    console.log('given start date : ', start);
-    console.log('given end date : ', end);
     let temp = start;
     start = end;
     end = temp;
-    console.log('switch : start=', start, ', end=', end);
+    // console.log('switch : start=', start, ', end=', end);
   }
 
   // On update le Graphe
   let url = generateGrafanaUrl(start, end);
-  document.getElementById("image1").setAttribute("src", url);
-}
-
-// Gestion evenement modification du select de l'humidité
-function onHumiditySelected(){
-  // On récupère les valeurs des différents select.
-  let v1 = $('select#humidityFromWeek').val(); // valeur du select
-  let v2 = $('select#humidityFromDay').val(); // valeur du select
-  let v3 = $('select#humidityFromHour').val(); // valeur du select
-  let v4 = $('select#humidityToWeek').val(); // valeur du select
-  let v5 = $('select#humidityToDay').val(); // valeur du select
-  let v6 = $('select#humidityToHour').val(); // valeur du select
-
-  // On calcul les temps en heure donnés par l'utilisateur
-  //    start : nombre d'heure dans le passé
-  let start = (parseInt(v1) * 7 * 24) + (parseInt(v2) * 24) + parseInt(v3);
-  //    end : nombre d'heure dans le passé devant être inférieur à 'start'
-  let end = (parseInt(v4) * 7 * 24) + (parseInt(v5) * 24) + parseInt(v6);
-
-  // Si end > start, on inverse les deux
-  if (end > start) {
-    console.log('given start date : ', start);
-    console.log('given end date : ', end);
-    let temp = start;
-    start = end;
-    end = temp;
-    console.log('switch : start=', start, ', end=', end);
-  }
-
-  // On update le Graphe
-  let url = generateGrafanaUrl(start, end);
-  document.getElementById("image2").setAttribute("src", url);
+  document.getElementById(ref).setAttribute("src", "");
+  document.getElementById(ref).setAttribute("src", url);
 }
 
 $( document ).ready(function() {
     // On effectue une opération quand le select est modifié
-    $('#temperatureFromWeek').on('change', onTemperatureSelected);
-    $('#temperatureFromDay').on('change', onTemperatureSelected);
-    $('#temperatureFromHour').on('change', onTemperatureSelected);
-    $('#temperatureToWeek').on('change', onTemperatureSelected);
-    $('#temperatureToDay').on('change', onTemperatureSelected);
-    $('#temperatureToHour').on('change', onTemperatureSelected);
-
-    $('#humidityFromWeek').on('change', onHumiditySelected);
-    $('#humidityFromDay').on('change', onHumiditySelected);
-    $('#humidityFromHour').on('change', onHumiditySelected);
-    $('#humidityToWeek').on('change', onHumiditySelected);
-    $('#humidityToDay').on('change', onHumiditySelected);
-    $('#humidityToHour').on('change', onHumiditySelected);
+    $('#submitButtonTemperature').click(function(){
+      onGraphSelected('temperature', 'image1');
+    });
+    $('#submitButtonHumidity').click(function(){
+      onGraphSelected('humidity', 'image2');
+    });
 });
